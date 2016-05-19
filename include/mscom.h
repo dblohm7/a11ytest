9ee6ab44482a03dd83698ec85b46e58319954316
Aaron Klotz <aklotz@mozilla.com>
2016-05-19 16:15:57 -0600

    main

---
+++ include/mscom.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_mscom_h
+#define mozilla_mscom_h
+
+#include <assert.h>
+#include <objbase.h>
+
+namespace mozilla {
+
+template<COINIT T>
+class COMApartmentRegion
+{
+public:
+  COMApartmentRegion()
+    : mInitResult(::CoInitializeEx(nullptr, T | COINIT_DISABLE_OLE1DDE))
+  {
+    // If this fires then we're probably mixing apartments on the same thread
+    assert(SUCCEEDED(mInitResult));
+  }
+
+  ~COMApartmentRegion()
+  {
+    if (SUCCEEDED(mInitResult)) {
+      ::CoUninitialize();
+    }
+  }
+
+  bool operator!() const
+  {
+    return FAILED(mInitResult);
+  }
+
+private:
+  COMApartmentRegion(const COMApartmentRegion&) = delete;
+  COMApartmentRegion& operator=(const COMApartmentRegion&) = delete;
+  COMApartmentRegion(COMApartmentRegion&&) = delete;
+  COMApartmentRegion& operator=(COMApartmentRegion&&) = delete;
+
+  HRESULT mInitResult;
+};
+
+typedef COMApartmentRegion<COINIT_APARTMENTTHREADED> STARegion;
+typedef COMApartmentRegion<COINIT_MULTITHREADED> MTARegion;
+
+} // namespace mozilla
+
+#endif // mozilla_mscom_h
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+#include "mscom.h"
+#include "winselect.h"
+#include "ArrayLength.h"
+
+#include <oleacc.h>
+
+#include <string>
+#include <stdio.h>
+using namespace std;
+
 int main(int argc, char* argv[])
 {
+  mozilla::STARegion sta;
+  WCHAR caption[256] = {0};
+  WCHAR className[256] = {0};
+  HWND hwnd = aspk::SelectWindow();
+  GetWindowText(hwnd, caption, ArrayLength(caption));
+  GetClassName(hwnd, className, ArrayLength(className));
+  printf("HWND: %p \"%S\" \"%S\"\n", hwnd, className, caption);
   return 0;
 }
 
